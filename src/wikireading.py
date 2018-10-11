cf476068027ef8b8294a86b50f50ceb72d2203f4
Cezar Sas <cezar.sas@gmail.com>
2018-10-11 15:14:44 +0200

    Minor useless edits

--- src/wikireading.py
+++ src/wikireading.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import multiprocessing
 
 from create_wikireading import build_vocabs, make_wikireading
 from merge_wikis import wikimerge
@@ -13,6 +14,12 @@ VOCABS = "vocabs"
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(module)s - %(levelname)s - %(message)s', level=logging.INFO)
     parser = argparse.ArgumentParser(description="Builds a dictionary mapping between Wikipedia IDs and Wikidata IDs")
+    parser.add_argument('--lang', help='Language code', dest="lang", required=True, type=str)
+    parser.add_argument('--ext_lang', help='Extended string for language', dest="ext_lang", required=True, type=str)
+    parser.add_argument('--locale', help="Locale used for date output", dest="locale", required=True, type=str)
+    parser.add_argument('--workers', help="Number of workers", dest="locale", default=multiprocessing.cpu_count() - 1,
+                        type=int)
+
     subparser = parser.add_subparsers(dest="subparser")
     merge = subparser.add_parser(MERGE)
     build = subparser.add_parser(BUILD)
@@ -31,4 +38,3 @@ if __name__ == '__main__':
 
     if args.subparser == BUILD:
         make_wikireading()
-
