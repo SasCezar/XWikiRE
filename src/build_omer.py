d9b7babdbef44df6996e053b4527f93ccb71226c
Cezar Sas <cezar.sas@gmail.com>
2018-10-19 13:34:56 +0200

    Working version

--- src/build_omer.py
+++ src/build_omer.py
@@ -1,5 +1,6 @@
 import logging
 import multiprocessing
+import re
 import time
 from functools import partial
 
@@ -7,72 +8,113 @@ from natural.date import compress
 from pymongo import MongoClient
 
 import config
-from utils import find_full_matches, find_matches, get_chunks
+from utils import get_chunks, is_sublist
 
 
 def distant_supervision(answer_sequence, entity_sequence, text_sequence, sentence_breaks):
     for start, end in zip([0] + sentence_breaks, sentence_breaks):
         sentence = text_sequence[start:end]
         # TODO If want to add aliases Cross product between aliases of answer and entity, then ANY for if statement
-        if answer_sequence in sentence and entity_sequence in sentence:
+        if is_sublist(answer_sequence, sentence) and is_sublist(entity_sequence, sentence):
             return start, end
 
     return False
 
 
-def build(page):
-    omer_doc = {"key": page['id'], "break_levels": page['break_levels'],
-                "string_sequence": page['string_sequence'], "paragraph_breaks": page['paragraph_breaks'],
-                "sentence_breaks": page['sentence_breaks'], "text": page['text'],
-                "entity_sequence": page['title_sequence'], "entity": page['title']}
+SEP_MAPPING = {
+    0: '',
+    1: ' ',
+    2: '\n',
+    3: ' ',
+    4: '\n\n'
+}
 
-    for prop in page['facts']:
-        question = page['properties'][prop]
-        omer_doc['question_string_sequence'] = question['label_sequence']
-        answer_string_sequence = []
-        answer_breaks = []
-        raw_answers = []
-        full_match_answer_location = []
-        answer_location = []
-        for fact in page['facts'][prop]:
-            if answer_string_sequence:
-                answer_breaks.append(len(answer_string_sequence))
-            raw_answers.append(fact['value'])
-            answer_sequence = fact['value_sequence']
-            answer_string_sequence += answer_sequence
-            full_match_answer_location.append(
-                find_full_matches(omer_doc["string_sequence"], answer_sequence))
-            answer_location.append(find_matches(omer_doc["string_sequence"], answer_sequence))
 
-            indexes = distant_supervision(answer_sequence, omer_doc['entity_sequence'],
-                                          omer_doc['string_sequence'], omer_doc['sentence_breaks'])
+def rebuild_sentence(start, end, tokens, breaks):
+    sentence = ""
+    for separator, token in zip(breaks[start:end], tokens[start:end]):
+        sentence += SEP_MAPPING[separator] + token
 
-        omer_doc['answer_string_sequence'] = answer_string_sequence
-        omer_doc['raw_answer_ids'] = raw_answers
-        omer_doc['answer_breaks'] = answer_breaks
-        omer_doc['full_match_answer_location'] = full_match_answer_location
+    return sentence.strip()
 
-    return
+
+BATCH_WRITE_SIZE = 500
+
+
+def clean_sentence(sentence):
+    sentence = re.sub("\n+", " ", sentence)
+    return sentence
+
+
+def build(limit, configs):
+    client = MongoClient(config.MONGO_IP, config.MONGO_PORT)
+    db = client[config.DB]
+    wikimerge = db[config.WIKIMERGE_COLLECTION]
+    omermerge = db[config.OMERMERGE_COLLECTION]
+
+    processed = []
+    n = 0
+    start_time = time.time()
+    for page in wikimerge.find({"id": {"$gte": limit[0], "$lte": limit[1]}}, {"_id": 0}):
+        omer_doc = {"id": page['id'], "break_levels": page['break_levels'], "string_sequence": page['string_sequence'],
+                    "sentence_breaks": page['sentence_breaks'], "text": page['text'],
+                    "label_sequence": page['label_sequence'], "label": page['label'], 'QA': {}}
+
+        for prop in page['facts']:
+            question = page['properties'][prop]
+            omer_doc['QA'][prop] = []
+            for fact in page['facts'][prop]:
+                answer_sequence = fact['value_sequence']
+
+                match_index = distant_supervision(answer_sequence, omer_doc['label_sequence'],
+                                                  omer_doc['string_sequence'], omer_doc['sentence_breaks'])
+
+                if not match_index:
+                    continue
+                start, end = match_index
+                sentence = rebuild_sentence(start, end, omer_doc['string_sequence'], omer_doc['break_levels'])
+                sentence = clean_sentence(sentence)
+                qa = (question['label'], sentence, fact['value'])
+
+                omer_doc['QA'][prop].append(qa)
+
+        processed.append(omer_doc)
+
+        if len(processed) > BATCH_WRITE_SIZE:
+            omermerge.insert_many(processed, ordered=False, bypass_document_validation=True)
+            n += len(processed)
+            processed = []
+
+    if processed:
+        omermerge.insert_many(processed, ordered=False, bypass_document_validation=True)
+        n += len(processed)
+
+    elapsed = int(time.time() - start_time)
+    return n, elapsed
 
 
 def build_omer(configs):
     client = MongoClient(config.MONGO_IP, config.MONGO_PORT)
     db = client[config.DB]
-    wikipedia = db[config.WIKIPEDIA_COLLECTION]
-    documents_id = list(wikipedia.find({}, {"wikidata_id": 1, "_id": 0}).sort("wikidata_id"))
+    wikipedia = db[config.WIKIMERGE_COLLECTION]
+    documents_id = list(wikipedia.find({}, {"id": 1, "_id": 0}).sort("id"))
     client.close()
     start_time = time.time()
     total = 0
-    pool = multiprocessing.Pool(config.NUM_WORKERS)
-    chunks = get_chunks(documents_id, config.CHUNK_SIZE, 'wikidata_id')
-    for n, elapsed in pool.map(partial(build, configs=configs), chunks):
-        total += n
-        part = int(time.time() - start_time)
-        logging.info("Processed {} ({} in total) documents in {} (running time {})".format(n, total,
-                                                                                           compress(elapsed),
-                                                                                           compress(part)))
+    chunks = get_chunks(documents_id, config.CHUNK_SIZE, 'id')
+    if config.NUM_WORKERS == 1:
+        for chunk in chunks:
+            build(chunk, {})
+    else:
+        pool = multiprocessing.Pool(config.NUM_WORKERS)
+        for n, elapsed in pool.map(partial(build, configs=configs), chunks):
+            total += n
+            part = int(time.time() - start_time)
+            logging.info("Processed {} ({} in total) documents in {} (running time {})".format(n, total,
+                                                                                               compress(elapsed),
+                                                                                               compress(part)))
 
-    pool.terminate()
+        pool.terminate()
     elapsed = int(time.time() - start_time)
     logging.info("Processed {} documents in {}".format(total, compress(elapsed)))
     return
